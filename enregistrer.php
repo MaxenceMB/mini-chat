a94f5bf6a920c6c9c425b235b16592137117f8ec
Maxence <133963749+MaxenceMB@users.noreply.github.com>
2024-04-08 12:56:44 +0200

    Multiple rooms and longer chats

--- enregistrer.php
+++ enregistrer.php
@@ -8,14 +8,16 @@ if(isset($_GET['pseudo']) && isset($_GET['phrase'])) {
     // Variables
     $pseudo = $_GET['pseudo'];
     $phrase = $_GET['phrase'];
+    $room   = $_GET['room'];
 
     // Préparation
-    $req = $pdo->prepare('INSERT INTO chatJS(userPseudo, contenu, horaire)
-                              VALUES(:pseudo, :contenu, :horaire)');
+    $req = $pdo->prepare('INSERT INTO chatJS(userPseudo, contenu, idRoom, horaire)
+                              VALUES(:pseudo, :contenu, :idRoom, :horaire)');
 
     // Requête
     $req->execute(array('pseudo'  => $pseudo,
                         'contenu' => $phrase,
+                        'idRoom'  => $room,
                         'horaire' => time()));
 
     echo "Success - Message ".$phrase." added to database.";
--- recuperer.php
+++ recuperer.php
@@ -3,22 +3,26 @@
 include "connexionBD.php";
 $pdo = createConnection();
 
-$stmt = $pdo->prepare("SELECT * FROM (SELECT * FROM chatJS ORDER BY horaire DESC LIMIT 10) AS sub ORDER BY idMessage ASC");
+$stmt = $pdo->prepare("SELECT * FROM (SELECT * FROM chatJS WHERE idRoom = :idRoom ORDER BY horaire DESC LIMIT 10) AS sub ORDER BY idMessage ASC");
 
 if ($stmt == false) {
     echo "PREPARE ERROR";
 } else {
-    $stmt->execute();
+    $stmt->execute([":idRoom" => $_POST["room"]]);
     while ($row = $stmt->fetch()) {
-        echo "<div class = 'msg'>";
-        echo "<p class = 'msg-text'> ".$row['contenu']."</p>";
-        if ($row['horaire'] > $timestamp = strtotime('today midnight')) {
-            echo "<p class = 'msg-info'> from ".$row['userPseudo']." at ".date('H:i A', $row['horaire'])."</p>";
-        } else {
-            echo "<p class = 'msg-info'> from ".$row['userPseudo']." at ".date('H:i A', $row['horaire'])." on ".date('m/d/Y', $row['horaire'])."</p>";
-        }
-        echo "</div>"; 
+        $messageDiv = "";
+        $id         =  $row['idMessage'];
+        $pseudo     = ($row['userPseudo'] == "") ? "Anonymous" : $row['userPseudo'];
+        $contenu    =  $row['contenu'];
+        $dateMsg    = ($row['horaire'] > $timestamp = strtotime('today midnight')) ? date('H:i A', $row['horaire']) : date('H:i A', $row['horaire'])." on ".date('d/m/Y', $row['horaire']);
+
+        $messageDiv .= "<div class = 'msg' data-msg = ".$id.">";
+        $messageDiv .= "<p class = 'msg-text'> ".$contenu."</p>";
+        $messageDiv .= "<p class = 'msg-info'> from ".$pseudo." at ".$dateMsg."</p>";
+        $messageDiv .= "</div>"; 
+
+        $messages[$row['idMessage']] = $messageDiv;
     }
+
+    echo json_encode($messages);
 }
-        
-?>
